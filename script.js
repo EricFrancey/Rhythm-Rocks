5dce812ba5362130eaf03371f69cccf10262931a
Eric Francey <eric.g.francey@gmail.com>
2022-06-08 19:46:53 -0400

    refactored button generation

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let dSpeedText = 1;
 var drumSpeed = 723;
 document.onkeydown = checkKey;
 
-
 function checkKey(e) {
    
   e = e || window.event;
@@ -65,127 +64,49 @@ function playSnare() {
 }
 
 function createKickButtons() {
+  for (let j = 0; j < 4; j++) {
 
-  let newBtn = [];
-  let snareCol = document.createElement('div');
-  snareCol.classList.add('col')
-  snareCol.innerHTML = '<h3><br>Kick</h3>'
-  document.body.appendChild(snareCol);
-  for (let i = 0; i < 16; i++) {
-    newBtn[i] = document.createElement('button');
-    newBtn[i].id = 'newBtn'
-    newBtn[i].classList.add('button-off')
-    newBtn[i].innerHTML = i+1;
-    document.body.appendChild(newBtn[i]);
-
-    newBtn[i].addEventListener('click', newColor)
-
-    function newColor() {
-      
-      if (newBtn[i].classList.contains('button-off')) {
-        newBtn[i].classList.remove('button-off')
-        newBtn[i].classList.add('button-on')
-      } else if (newBtn[i].classList.contains('button-on')){
-        newBtn[i].classList.add('button-off')
-        newBtn[i].classList.remove('button-on')
-      }
+    let newBtn = [];
+    let snareCol = document.createElement('div');
+    snareCol.classList.add('col')
+    if (j == 0) {
+      snareCol.innerHTML = '<h3><br>Kick</h3>'
+    } else if (j ==1) {
+      snareCol.innerHTML = '<h3><br>Snare</h3>'
+    } else if (j ==2) {
+      snareCol.innerHTML = '<h3><br>Hihat</h3>'
+    } else if (j ==3) {
+      snareCol.innerHTML = '<h3><br>Openhat</h3>'
     }
-  }
-}
-
-function createSnareButtons() {
-
-  let newBtn = [];
-  let snareCol = document.createElement('div');
-  snareCol.classList.add('col')
-  snareCol.innerHTML = '<h3><br>Snare</h3>'
-  document.body.appendChild(snareCol);
-  for (let i = 0; i < 16; i++) {
-    newBtn[i] = document.createElement('button');
-    newBtn[i].id = 'newBtn'
-    newBtn[i].classList.add('button-off')
-    newBtn[i].innerHTML = i+1;
-    document.body.appendChild(newBtn[i]);
-
-    newBtn[i].addEventListener('click', newColor)
-
-    function newColor() {
-
-      if (newBtn[i].classList.contains('button-off')) {
-        newBtn[i].classList.remove('button-off')
-        newBtn[i].classList.add('button-on')
-      } else if (newBtn[i].classList.contains('button-on')){
-        newBtn[i].classList.add('button-off')
-        newBtn[i].classList.remove('button-on')
-      }
-    }
-  }
-}
-
-function createOpenhatButtons() {
-
-  let newBtn = [];
-  let snareCol = document.createElement('div');
-  snareCol.classList.add('col')
-  snareCol.innerHTML = '<h3><br>Openhat</h3>'
-  document.body.appendChild(snareCol);
-  for (let i = 0; i < 16; i++) {
-    newBtn[i] = document.createElement('button');
-    newBtn[i].id = 'newBtn';
-    newBtn[i].classList.add('button-off')
-    newBtn[i].innerHTML = i+1;
-    document.body.appendChild(newBtn[i]);
-
-    newBtn[i].addEventListener('click', newColor)
-
-    function newColor() {
-
-      if (newBtn[i].classList.contains('button-off')) {
-        newBtn[i].classList.remove('button-off')
-        newBtn[i].classList.add('button-on')
-      } else if (newBtn[i].classList.contains('button-on')){
-        newBtn[i].classList.add('button-off')
-        newBtn[i].classList.remove('button-on')
-      }
-    }
-  }
-}
   
-function createHihatButtons() {
 
-  let newBtn = [];
-  let snareCol = document.createElement('div');
-  snareCol.classList.add('col')
-  snareCol.innerHTML = '<h3><br>Hihat</h3>'
-  document.body.appendChild(snareCol);
-  for (let i = 0; i < 16; i++) {
-    newBtn[i] = document.createElement('button');
-    newBtn[i].id = 'newBtn';
-    newBtn[i].classList.add('button-off')
-    newBtn[i].innerHTML = i+1;
-    document.body.appendChild(newBtn[i]);
+    document.body.appendChild(snareCol);
+    for (let i = 0; i < 16; i++) {
+      newBtn[i] = document.createElement('button');
+      newBtn[i].id = 'newBtn'
+      newBtn[i].classList.add('button-off')
+      newBtn[i].innerHTML = i+1;
+      document.body.appendChild(newBtn[i]);
 
-    newBtn[i].addEventListener('click', newColor)
+      newBtn[i].addEventListener('click', newColor)
 
-    function newColor() {
-
-      if (newBtn[i].classList.contains('button-off')) {
-        newBtn[i].classList.remove('button-off')
-        newBtn[i].classList.add('button-on')
-      } else if (newBtn[i].classList.contains('button-on')){
-        newBtn[i].classList.add('button-off')
-        newBtn[i].classList.remove('button-on')
+      function newColor() {
+      
+        if (newBtn[i].classList.contains('button-off')) {
+          newBtn[i].classList.remove('button-off')
+          newBtn[i].classList.add('button-on')
+        } else if (newBtn[i].classList.contains('button-on')){
+          newBtn[i].classList.add('button-off')
+          newBtn[i].classList.remove('button-on')
+        }
       }
     }
   }
 }
-     
+ 
 function init() {
 
   createKickButtons();
-  createSnareButtons();
-  createHihatButtons();
-  createOpenhatButtons();
 }
 
 init();
